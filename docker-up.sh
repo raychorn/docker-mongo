d4760f7ee6ada47c55ca4447837628ea23debe4e
Ray C Horn <raychorn@gmail.com>
2021-01-06 21:28:54 -0700

    updates.

--- docker-up.sh
+++ docker-up.sh
@@ -4,3 +4,8 @@ chmod -R 0777 ./mongodb
 docker-compose up -d --remove-orphans
 CID=$(docker ps -qf "name=mongodb")
 echo "CID=$CID"
+if [[ ! $CID. == . ]]
+then
+    echo "Update $CID"
+    docker update --restart unless-stopped --cpus="2.5" --memory="2g" --memory-swap="2.5g" $CID
+fi
